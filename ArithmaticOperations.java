0ae851ce34d1879a4537635c9238bdccf18338fb
R K Sobithan <sobikrishnan001@gmail.com>
2025-03-31 15:35:16 +0530

    Add files via upload

---
+++ ArithmaticOperations.java
@@ -0,0 +1,27 @@
+public class ArithmaticOperations {
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 5;
+        int sum = a+b;
+
+        System.out.println(3 + 8 + " Extra : 2 + 4 " + 9 + 8); // Apple
+        System.out.println(a + b);
+        System.out.println(3 + 5);
+        System.out.println(5 - 2);
+
+        System.out.println("Addition : " + a + " + " + b + " = " + sum);
+
+        int difference = a-b;
+
+        System.out.println("Substraction : " + a + " - " + b + " = " + difference);
+
+        int product = a*b;
+
+        System.out.println("Multiplication : " + a + " * " + b + " = " + product);
+
+        int quotient = a/b;
+
+        System.out.println("Division : " + a + " / " + b + " = " + quotient);
+    }
+}
